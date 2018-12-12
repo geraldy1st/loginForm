faf7a614271ea6c53abcba1ecabfbe886de50410
geraldy1st <geraldy.leondas@gmail.com>
2018-12-12 13:03:17 +0100

    maj

--- secret.php
+++ secret.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../style.css">
     <title>User page</title>
 </head>
 <body>
@@ -14,7 +15,7 @@
         echo '<iframe width="643" height="362" src="https://www.youtube.com/embed/rDW5epw8c3g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
             echo "<p>" . $_POST["password"] . " was the password.</p>";
     }else{
-        echo "<p>Login fail..! <br>try root as an username...</p>";
+        echo "<p>Login fail..! <br>try root as username...</p>";
    }
      ?>
 
